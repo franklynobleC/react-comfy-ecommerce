b22ecd6bee7d496761335b336c4dc923f743de87
franklynobleC <franklyessien01@gmail.com>
2023-08-05 22:26:04 +0100

    add nav-links

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Navbar, Sidebar, Footer } from './components'
+
+import {
+  Home,
+  SingleProduct,
+  About,
+  CheckOut,
+  Error,
+  PrivateRoute,
+  Products,
+  Cart
+} from './pages'
+
+// import styled from 'styled-components'
+// import Testing from './Testing';
+// how  to  style a component for  my reference
+// const Button = styled.button`
+//   background: green;
+//   color: white;
+//   padding: 10px;
+//   content: center;
+// `
+
+// const Container = styled.div`
+//   background: red;
+//   color: white;
+//   font-size: 2rem;
+//   .hero {
+//     font-size: 8rem;
+
+//     color-scheme: black;
+//   }
+// `
+function App () {
+  return (
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Switch>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+        <Route exact path='/about'>
+          <About />
+        </Route>
+        <Route exact path='/cart'>
+          <Cart />
+        </Route>
+        <Route exact path='/products'>
+          <Products />
+        </Route>
+        <Route exact path='/checkout'>
+          <CheckOut />
+        </Route>
+        <Route exact path='/products/:id' children={<SingleProduct />}>
+          <SingleProduct   />
+          <Route exact path='*'>
+            <Error />
+          </Route>
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  )
+}
+
+export default App
---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Home from './HomePage'
+import Products from './ProductsPage'
+
+import About from './AboutPage'
+import Cart from './CartPage'
+import Error from './ErrorPage'
+import CheckOut from './CheckoutPage'
+import PrivateRoute from './PrivateRoute'
+import SingleProduct from './SingleProductPage'
+
+export {
+  Home,
+  Products,
+  SingleProduct,
+  Cart,
+  About,
+  CheckOut,
+  Error,
+  PrivateRoute
+}
