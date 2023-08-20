96746fe1ad8926487bced8806af597b1be67c285
franklynobleC <franklyessien01@gmail.com>
2023-08-20 01:12:47 +0100

    add to cart

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -6,6 +6,7 @@ import { useCartContext } from '../context/cart_context'
 import AmountButtons from './AmountButtons'
 
 const AddToCart = ({ product }) => {
+  const { addToCart } = useCartContext()
   const { id, stock, colors } = product
 
   const [mainColor, SetMainColor] = useState(colors[0])
@@ -67,7 +68,11 @@ const AddToCart = ({ product }) => {
           decrease={decrease}
         />
 
-        <Link to='/cart' className='btn'>
+        <Link
+          to='/cart'
+          className='btn'
+          onClick={() => addToCart(id, mainColor, amount, product)}
+        >
           add to cart
         </Link>
       </div>
--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -5,16 +5,30 @@ import {
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
   CLEAR_CART,
-  COUNT_CART_TOTALS,
+  COUNT_CART_TOTALS
 } from '../actions'
 
-const initialState = {}
+const initialState = {
+  cart: [],
+  total_items: 0,
+  total_amount: 0,
+  shipping_fee: 534
+}
 
 const CartContext = React.createContext()
 
 export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+
+  //add to cart function
+  const addToCart = (id, color, amount, product) => {
+    dispatch({ type: ADD_TO_CART, payload: { id, color, amount, product } })
+  }
+
   return (
-    <CartContext.Provider value='cart context'>{children}</CartContext.Provider>
+    <CartContext.Provider value={{ ...state, addToCart }}>
+      {children}
+    </CartContext.Provider>
   )
 }
 // make sure use
